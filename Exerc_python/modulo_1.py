3702fc0bed53be8abfbdab1d7e202c66d9593cad
Kaylane <kaycoutinho387@gmail.com>
2024-11-12 11:58:36 -0300

    Exercicio-Python

---
+++ Exerc_python/modulo_1.py
@@ -0,0 +1,7 @@
+def saudacao(nome):
+    """
+    A função saudacao exibe uma mensagem de recepção.
+    ele pede um parâmetro(argumento) chamado nome.
+    Você deve passar o nome de alguém
+    """
+    print(f"Olá, {nome}")
---
+++ Exerc_python/usar_modulo_1.py
@@ -0,0 +1,4 @@
+import modulo_1
+# from modulo_1 import saudacao
+
+modulo_1.saudacao("Roberta")
