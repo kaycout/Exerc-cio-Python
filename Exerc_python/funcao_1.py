f5c5672dd9c21a1657ecbd9b295d12348ec1a7f8
Kaylane <kaycoutinho387@gmail.com>
2024-11-12 11:49:05 -0300

    Exercicio-Python

---
+++ Exerc_python/funcao_1.py
@@ -0,0 +1,11 @@
+def linhaHorizontal():
+    """
+    A funcão linhaHorizontal adiciona uma linha tracejada na tela
+    de console para separar os elementos de saída
+    """
+    print("==========================================================================")
+
+print("Bem-vindo ao Sistema!")
+linhaHorizontal()
+print("Você está no sistema de funções.")
+linhaHorizontal()
---
+++ Exerc_python/funcao_2.py
@@ -0,0 +1,18 @@
+def soma(n1,n2):
+    return n1 + n2
+
+def produto(n1,n2):
+    return n1 * n2
+
+def restoDivisão(n1,n2):
+    return n1 % n2
+
+def separarResultados(texto):
+    print("--------------------------- O resto de "+texto+"--------------------------------")
+
+
+print("Olá, seja bem vindo ao programa de calculos")
+separarResultados("Multiplicacao")
+print("O resultado da soma entre os números 5 e 10 é "+str(soma(5, 10)))
+separarResultados("Resto da divisão")
+print("Entre os números 5 e 10 é "+str(restoDivisão(5,10)))
